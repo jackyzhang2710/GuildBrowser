dde087a24076077227a34fb5f6d3561dc0dbf7e8
Jacky <jackyzhang2710@sina.com>
2015-08-30 12:13:22 +0800

    commit second time

---
+++ GuildBrowser/TestClass.h
@@ -0,0 +1,21 @@
+//
+//  TestClass.h
+//  GuildBrowser
+//
+//  Created by JackyCheung on 15/8/30.
+//  Copyright (c) 2015年 SkyStudio. All rights reserved.
+//
+
+#ifndef GuildBrowser_TestClass_h
+#define GuildBrowser_TestClass_h
+
+#import <Foundation/Foundation.h>
+
+
+@interface TestClass : NSObject
+
+@end
+
+
+
+#endif
---
+++ GuildBrowser/TestClass.m
@@ -0,0 +1,10 @@
+//
+//  TestClass.m
+//  GuildBrowser
+//
+//  Created by JackyCheung on 15/8/30.
+//  Copyright (c) 2015年 SkyStudio. All rights reserved.
+//
+
+
+#import "TestClass.h"
